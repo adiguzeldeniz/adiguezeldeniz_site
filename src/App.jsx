b4db0984bdace8983ee0766a77b299c12139af43
adiguzeldeniz <adiguzeldeniz@outlook.de>
2025-12-21 11:24:09 +0100

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Mail, Phone, Linkedin, ExternalLink, Award, BookOpen, GraduationCap, Microscope, Calendar, FileDown } from 'lucide-react'
 
 function App() {
